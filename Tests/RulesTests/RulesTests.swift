25ef78c92689a26e5030b1e7457a7a2f5a81dd65
Dandy Griffin <dan@contagious.nu>
2024-04-10 10:46:28 -0500

    Building out api

--- Tests/RulesTests/RulesTests.swift
+++ Tests/RulesTests/RulesTests.swift
@@ -4,13 +4,13 @@ import XCTest
 
 
 final class RulesTests: XCTestCase {
-    func testSimpleExample() throws {
+    func testSimpleBooleanExample() throws {
         struct State {
             var distance: Int
         }
 
         enum Fact {
-            case playerClose
+            case playerInSight
         }
 
 
@@ -19,18 +19,57 @@ final class RulesTests: XCTestCase {
         let closeToPlayerRule = Rule<State, Fact> { editor in
             return editor.state.distance < 50
         } action: { editor in
-            editor.assert(.playerClose, grade: 1.0)
+            editor.assert(.playerInSight, grade: 1.0)
         }
 
         let farFromPlayerRule = Rule<State, Fact> { editor in
             return editor.state.distance >= 50
         } action: { editor in
-            editor.retract(.playerClose, grade: 1.0)
+            editor.retract(.playerInSight, grade: 1.0)
         }
 
+        ruleSystem.add(rules: [closeToPlayerRule, farFromPlayerRule])
+
         let outcome = ruleSystem.evaluate(state: State(distance: 20))
-        let grade = outcome.grade(for: .playerClose)
+        let grade = outcome.grade(for: .playerInSight)
         XCTAssertEqual(grade, 1.0)
+
+        let outcome2 = ruleSystem.evaluate(state: State(distance: 75))
+        let grade2 = outcome2.grade(for: .playerInSight)
+        XCTAssertEqual(grade2, 0)
+    }
+
+    func testSimpleFuzzyExample() throws {
+        struct State {
+            var distance: Double
+            var maxSightDistance: Double
+        }
+
+        enum Fact {
+            case playerNear
+        }
+
+        let ruleSystem = RuleSystem<State, Fact>()
+
+        let closeToPlayerRule = Rule<State, Fact> { editor in
+            return editor.state.distance <= 30
+        } action: { editor in
+            // The closer, the higher the value
+            let isNearValue = Double(1.0 - editor.state.distance / editor.state.maxSightDistance)
+            editor.assert(.playerNear, grade: isNearValue)
+        }
+
+        ruleSystem.add(rule: closeToPlayerRule)
+
+        let state = State(distance: 15, maxSightDistance: 30)
+        let outcome = ruleSystem.evaluate(state: state)
+        let grade = outcome.grade(for: .playerNear)
+        XCTAssertEqual(grade, 0.5)
+
+        let state2 = State(distance: 35, maxSightDistance: 30)
+        let outcome2 = ruleSystem.evaluate(state: state2)
+        let grade2 = outcome2.grade(for: .playerNear)
+        XCTAssertEqual(grade2, 0)
     }
 
 //    func testTertiaryExample() throws {
