bea5061ef339be1b12f05b352592dc52cf5e3ed3
Dandy Griffin <dan@contagious.nu>
2024-04-10 22:53:44 -0500

    Tests reevaluation

--- Tests/RulesTests/RulesTests.swift
+++ Tests/RulesTests/RulesTests.swift
@@ -70,6 +70,40 @@ final class RulesTests: XCTestCase {
         XCTAssertEqual(grade2, 0)
     }
 
+    func testReevaluatesRulesetExample() throws {
+        struct State { }
+
+        enum Fact {
+            case didRun
+        }
+
+
+        let ruleSystem = RuleSystem<State, Fact>()
+
+        // Skipped until the second rule has been run
+        let firstRule = Rule<State, Fact>(salience: 0) { editor in
+            // Only run if we have an asserted fact.
+            // We then retract that fact
+            return editor.assertedFactsAndGrades.keys.contains(.didRun)
+        } action: { editor in
+            editor.retract(.didRun, grade: 1.0)
+        }
+
+        // After this runs, the evaluation is clear to run the first rule.
+        let secondRule = Rule<State, Fact>(salience: 1) { editor in
+            return true
+        } action: { editor in
+            editor.assert(.didRun, grade: 1.0)
+        }
+
+        ruleSystem.add(rules: [firstRule, secondRule])
+
+        let result2 = ruleSystem.evaluate(state: State())
+        let grade2 = result2.grade(for: .didRun)
+        XCTAssertEqual(grade2, 0)
+    }
+
+
 //    func testTertiaryExample() throws {
 //        struct State {
 //            var health: Int
